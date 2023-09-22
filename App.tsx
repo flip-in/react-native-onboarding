bec698e876afaf9676832ef418624582fbea89f2
Will Price <pricew86@gmail.com>
2023-09-22 14:17:51 +0200

    add persist query

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import PointsListScreen from './src/screens/PointsListScreen';
 import MapScreen from './src/screens/MapScreen';
+import {queryClient, clientPersister} from './lib/clientPersister';
+import {PersistQueryClientProvider} from '@tanstack/react-query-persist-client';
 // import {
 //   // SafeAreaView,
 //   // ScrollView,
@@ -17,12 +19,16 @@ const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Points List">
-        <Stack.Screen name="Points List" component={PointsListScreen} />
-        <Stack.Screen name="Map" component={MapScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{persister: clientPersister}}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Points List">
+          <Stack.Screen name="Points List" component={PointsListScreen} />
+          <Stack.Screen name="Map" component={MapScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PersistQueryClientProvider>
   );
 }
 
